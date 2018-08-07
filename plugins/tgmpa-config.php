b2e617db626b691c0c0833ea56788d7952813d0e
Tofandel <adrien.foulon.lfgeb@gmail.com>
2018-08-07 14:44:56 +0200

    1.5

--- plugins/tgmpa-config.php
+++ plugins/tgmpa-config.php
@@ -64,11 +64,11 @@ add_action( 'tgmpa_register',
 				// The plugin name.
 				'slug'               => 'wplusplus-core',
 				// The plugin slug (typically the folder name).
-				'source'             => 'https://github.com/Tofandel/wplusplus-core/archive/v1.3.zip',
+				'source'             => 'https://github.com/Tofandel/wplusplus-core/archive/v1.5.zip',
 				// The plugin source.
 				'required'           => true,
 				// If false, the plugin is only 'recommended' instead of required.
-				'version'            => '1.3',
+				'version'            => '1.5',
 				// E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
 				'force_activation'   => false,
 				// If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
@@ -76,7 +76,7 @@ add_action( 'tgmpa_register',
 				// If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
 				//'external_url'       => 'https://github.com/tofandel/wplusplus-core',
 				// If set, overrides default API URL and points to an external URL.
-				//'is_callable'        => '',
+				'is_callable'        => [ \Tofandel\WPlusPlusCore::class, 'checkCompatibility' ],
 				'class_exists'       => \Tofandel\WPlusPlusCore::class
 				// If set, this callable will be be checked for availability to determine if a plugin is active.
 			),
